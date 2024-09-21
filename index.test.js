310331291f96d1d7ae62a9d0b871e6d9cdf26dfd
Garpirik <ggarpir@gmail.com>
2024-09-21 02:20:44 +0200

    added trigonometry functions

--- index.test.js
+++ index.test.js
@@ -54,6 +54,33 @@ const rootPow =  ( degrees , ...args) =>{
     }
 }   
 
+const sin = (a) =>{
+    return Math.sin(a);
+}
+const cos = (a) =>{
+    return Math.cos(a);
+}
+const tang = (a) =>{
+    return Math.tan(a);
+}
+
+const arcsin = (a) =>{
+    return Math.asin(a);
+}
+const arccos = (a) =>{
+    return Math.acos(a);
+}
+
+const arctang = (a) =>{
+    return Math.atan(a);
+}
+
+const log = (a) =>{
+    return Math.log(a);
+}
+const ln = (a) =>{
+    return Math.log10(a);
+}
 const addNumber = () =>{
 
 }
@@ -120,4 +147,30 @@ describe("Function calc", () =>{
     test("Value pi?", ()=>{
         expect(pi).toBe(3.14)
     })
+    test("sin", () =>{
+        expect(sin(30)).toBe(-0.9880316240928618)
+    })
+    test("cos", () =>{
+        expect(cos(30)).toBe(0.15425144988758405)
+    })
+    test("tang", () =>{
+        expect(tang(30)).toBe(-6.405331196646276)
+    })
+    test("arcsin", () =>{
+        expect(arcsin(30)).toBe(NaN)
+    })
+    test("arccos", () =>{
+        expect(arccos(30)).toBe(NaN)
+    })
+    test("arctang", () =>{
+        expect(arctang(30)).toBe(1.5374753309166493)
+    })
+
+    test("log", () =>{
+        expect(log(2)).toBe(0.6931471805599453)
+    })
+    test("ln", () =>{
+        expect(ln(10)).toBe(1)
+    })
+
 })
